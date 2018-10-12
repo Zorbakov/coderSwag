3ec5c543f2f1187c1d18330032962db0fa94b919
Barry de Wit <b.dewit93@gmail.com>
2018-10-12 10:50:33 +0200

    Initial commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Barry de Wit on 12/10/2018.
+//  Copyright © 2018 Barry de Wit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
